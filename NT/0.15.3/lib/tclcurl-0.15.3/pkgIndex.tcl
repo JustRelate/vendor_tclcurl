3b7804972637f689abb89a69ba7ddb0fc727c564
Thomas Ritz <tritz@infopark.de>
2008-08-29 10:30:22 +0200

    tbz2 for new packaging

--- NT/0.15.3/lib/tclcurl-0.15.3/pkgIndex.tcl
+++
@@ -1,5 +0,0 @@
-#
-# Tcl package index file
-#
-package ifneeded TclCurl 0.15.3 \
-    [list load [file join $dir TclCurl0153.dll]]\n[list source [file join $dir tclcurl.tcl]]
--- solaris/0.15.3/lib/tclcurl-0.15.3/pkgIndex.tcl
+++
@@ -1,5 +0,0 @@
-#
-# Tcl package index file
-#
-package ifneeded TclCurl 0.15.3 \
-    [list load [file join $dir libTclCurl0.15.3.so]]\n[list source [file join $dir tclcurl.tcl]]
